fbf4ad08dd1c4a668162e52ab7bda884e47c2c6b
Dahele <h_dahele@tss-hr420-dm26.fc.ca>
2018-05-30 13:46:56 -0400

    js updated

--- js/main.js
+++ js/main.js
@@ -6,4 +6,17 @@
 	multi line
 	comment
 	 */
-})
+	
+	var words = "This is a staring of letters";
+	var num = 1;
+	console.log(words);
+	console.log("number of times of visited "+num);
+	var teams = ["knights","caps","leafs"];
+	console.log(teams[1]); //1st position of an array is 0
+	function i_am_a_function (argument) {
+		console.log("Form i_am_a_function()");
+	}
+window.addEventListener("load",i_am_a_function, false);
+
+
+})();
